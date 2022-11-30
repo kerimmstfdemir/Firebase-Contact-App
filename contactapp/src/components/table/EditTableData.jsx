9ee9c5ebf9271922a98a92f59b166f1e133c2ac0
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-30 20:06:17 +0300

    completed data editing processes

--- contactapp/src/components/table/EditTableData.jsx
+++ contactapp/src/components/table/EditTableData.jsx
@@ -1,36 +1,62 @@
-const EditTableData = () => {
-  return (
-    <div className="modal fade" id="editData" tabIndex={-1}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Modal title</h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            />
-          </div>
-          <div className="modal-body">
-            <p>Modal body text goes here.</p>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary" data-bs-dismiss="modal">
-              Save changes
-            </button>
-          </div>
+import { getDatabase, ref, update } from "firebase/database";
+import { useContext } from "react";
+import { ContactContext } from "../../App";
+import app from "../../utils/firebase";
+
+const EditTableData = ({ dataId }) => {
+    const { userContact, setUserContact } = useContext(ContactContext)
+
+    const handleUpdate = (userContact) => {
+        const database = getDatabase(app);
+        const dataRef = ref(database, `contacts/${dataId}`)
+        update(dataRef, userContact)
+    }
+    console.log(userContact);
+    return (
+        <div className="modal fade" id="editData" tabIndex={-1}>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Edit Contact</h5>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        />
+                    </div>
+                    <div className="modal-body">
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="contactName" className="form-label"><b>Name :</b></label>
+                                <input type="text" className="form-control" id="contactName" value={userContact.name} onChange={(e) => setUserContact({...userContact, name:e.target.value})}/>
+                                <label htmlFor="contactPhone" className="form-label mt-2"> <b>Phone Number :</b></label>
+                                <input type="tel" className="form-control" id="contactPhone" value={userContact.phoneNumber} onChange={(e) => setUserContact({...userContact, phoneNumber:e.target.value})}/>
+                                <label htmlFor="contactGender" className="form-label mt-2"><b>Gender :</b></label>
+                                <select id="contactGender" class="form-select form-select-md mb-1" aria-label=".form-select-md example" value={userContact.gender} onChange={(e) => setUserContact({...userContact, gender:e.target.value})}>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={() => handleUpdate(userContact)}>
+                            Save changes
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default EditTableData;
--- contactapp/src/components/table/Table.jsx
+++ contactapp/src/components/table/Table.jsx
@@ -2,13 +2,16 @@ import { TableStyledIncludingDiv } from "./Table.styled"
 import "./table.css"
 import { getDatabase, onValue, ref, remove } from "firebase/database"
 import app from "../../utils/firebase"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import DeleteIcon from '@mui/icons-material/Delete';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import EditTableData from "./EditTableData"
+import { ContactContext } from "../../App"
 
 const Table = () => {
     const [contactList, setContactList] = useState([]);
+    const { userContact, setUserContact } = useContext(ContactContext)
+    const [dataId, setDataId] = useState("")
 
     const handleDeleteData = (id) => {
         const database = getDatabase(app);
@@ -31,15 +34,16 @@ const Table = () => {
         })
     },[])
 
+    console.log(userContact);
 
   return (
     <div style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
     <TableStyledIncludingDiv>
         <p>CONTACTS</p>
-        <table className="text-center table table-striped table-hover" style={{width:"100%"}}>
+        <table className="table table-hover text-center" style={{width:"100%"}}>
             <thead>
                 <tr>
-                    <th>Username</th>
+                    <th>Name</th>
                     <th>Phone Number</th>
                     <th>Gender</th>
                     <th>Delete</th>
@@ -48,6 +52,10 @@ const Table = () => {
             </thead>
             {contactList.map((item)=>{
                 const {id, name, gender, phoneNumber} = item
+                const handleEditButton = () => {
+                    setUserContact({...userContact, name:name, gender:gender, phoneNumber:phoneNumber})
+                    setDataId(id)
+                }
                 return(
                     <tbody>
                         <tr>
@@ -55,14 +63,14 @@ const Table = () => {
                             <td>{phoneNumber}</td>
                             <td>{gender}</td>
                             <td><DeleteIcon style={{color:"#A62B1F", cursor:"pointer"}} onClick={() => handleDeleteData(id)}/></td>
-                            <td><BorderColorIcon data-bs-toggle="modal" data-bs-target="#editData" style={{color:"gray", cursor:"pointer"}}/></td>
+                            <td><BorderColorIcon data-bs-toggle="modal" data-bs-target="#editData" style={{color:"gray", cursor:"pointer"}} onClick={handleEditButton}/></td>
                         </tr>
                     </tbody>
                 )
             })}
         </table>
     </TableStyledIncludingDiv>
-    <EditTableData />
+    <EditTableData dataId={dataId}/>
     </div>
   )
 }
